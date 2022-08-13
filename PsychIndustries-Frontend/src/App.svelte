d1ad73b8fd23c42e5aca81418921a85ad28e4f3e
mienghabehardien <>
2022-08-13 23:01:41 +0200

    navbar

--- PsychIndustries-Frontend/src/App.svelte
+++ PsychIndustries-Frontend/src/App.svelte
@@ -1,45 +1,34 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
+  import Navbar from "./lib/Navbar.svelte";
+
+  let name = "Miengha";
+
+  $: uppercaseName = name.toUpperCase();
+
+  function changeName(event) {
+    name = event.target.value;
+  }
 </script>
 
 <main>
+  <Navbar />
   <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+    <h1>{uppercaseName}</h1>
+<!--    <input type="text" value="{name}" on:input={changeName} />-->
+
+    <input type="text" bind:value="{name}"/>
+
+    <!--    <button on:click={changeName}>Change Name</button>-->
 
   <div class="card">
     <Counter />
   </div>
+  </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
---
+++ PsychIndustries-Frontend/src/lib/Navbar.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Test1 from "../pages/Test1.svelte";
+    import Test2 from "../pages/Test2.svelte";
+
+    export let menu = 1;
+</script>
+
+<style>
+    ul#menu li{
+        display : inline;
+    }
+</style>
+
+<ul id="menu">
+    <li><a href="/" on:click|preventDefault={() => (menu = 1)}>Test1</a></li> |
+    <li><a href="/" on:click|preventDefault={() => (menu = 2)}>Test2</a></li>
+</ul>
+
+{#if menu === 1}
+    <Test1 />
+{:else if menu === 2}
+    <Test2 />
+{:else}
+    <h1>
+        Page Not Found
+    </h1>
+{/if}
+
